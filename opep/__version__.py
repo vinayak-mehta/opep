d3343a9dbf4c41698b282ee225fa36975e730f54
Vinayak Mehta <vmehta94@gmail.com>
2020-10-01 19:41:59 +0530

    Update peps and bump version

--- opep/__version__.py
+++ opep/__version__.py
@@ -3,7 +3,7 @@
 __title__ = "opep"
 __description__ = "Open PEPs on your terminal."
 __url__ = "https://github.com/vinayak-mehta/opep"
-__version__ = "2020.09.26"
+__version__ = "2020.10.01"
 __author__ = "Vinayak Mehta"
 __author_email__ = "vmehta94@gmail.com"
 __license__ = "Apache 2.0"
